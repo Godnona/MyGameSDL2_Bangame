65dcf40beb07262db4bff36a24994565eefeb2f8
GodNona <datanhpham181208@gmail.com>
2025-01-01 15:33:14 +0700

    test input

--- MyGameSDL2/src/GameManager.cpp
+++ MyGameSDL2/src/GameManager.cpp
@@ -5,6 +5,8 @@ void BanZ::GameManager::Init()
 {
 	const auto banGame = BanGame::Get();
 
+	position = VECTOR2(WIDTH, HEIGHT);
+
 	// load background
 	background = new GameObject();
 	background->LoadImg(banGame, PATH_BACKGROUND);
@@ -14,11 +16,7 @@ void BanZ::GameManager::Init()
 	//map->InitFormat(PATH_MAP_FORMAT);
 	//map->InitTile(banGame);
 
-	// init anim
-	manageAnim = banGame->CreateAnimator();
-	runLeftAnim = banGame->CreateAnimation("RunLeft", PATH_PLAYER_RUN_LEFT);
-	banGame->AddAnimationToAnimator(manageAnim, runLeftAnim);
-	banGame->SetAnimation(manageAnim, PLAYERSTATE::RUN_LEFT, false);
+	
 
 }
 
@@ -27,18 +25,17 @@ void BanZ::GameManager::Render() const
 	const auto banGame = BanGame::Get();
 
 	// render background
-	background->RenderImg(banGame, { WIDTH / 2, HEIGHT / 2 }, { WIDTH, HEIGHT });
+	background->RenderImg(banGame, { position.x / 2, position.y / 2 }, { WIDTH, HEIGHT });
 	
 	// render map
 	//map->Render(banGame);
 
-	// render anim
-	banGame->PlayAnimation(manageAnim, { WIDTH / 2, HEIGHT / 2 }, BLOCK_SIZE, 0, {1.0f, 1.0f, 0.0f, 1.0f});
 
 }
 
 void BanZ::GameManager::Update(float deltaTime, BanGame* banGame)
 {
-	// update anim
-	banGame->UpdateAnimation(manageAnim, deltaTime);
+	if (banGame->GetPress(KeyBoard::Keys::W))
+		position += VECTOR2(0, 1);
+
 }
